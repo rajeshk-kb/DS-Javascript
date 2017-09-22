1fe26300bd3029fc0f571b22ff235d387708f0aa
User <user@Apples-MacBook-Pro.local>
2017-09-22 17:31:05 +0530

    Making of BST

---
+++ Tree/create_binary_tree.js
@@ -0,0 +1,51 @@
+function Node(val){
+	this.value = val;
+	this.left = null;
+	this.right = null;
+}
+
+function BinarySearchTree(){
+	this.root = null
+}
+
+BinarySearchTree.prototype.insert = function(value){
+
+	if(!this.root){
+		this.root = new Node(value);
+		return;
+	}
+
+	var currentRoot = this.root;
+
+	while(currentRoot){
+		if(value <= currentRoot.value){
+			if(!currentRoot.left){
+				currentRoot.left = new Node(value);
+				break;
+			}else{
+				currentRoot = currentRoot.left;
+			}
+		}else{
+
+			if(!currentRoot.right){
+				currentRoot.right = new Node(value);
+				break;
+			}else{
+				currentRoot = currentRoot.right;
+			}
+		}
+	}
+
+}
+
+var bst = new BinarySearchTree();
+bst.insert(3);
+bst.insert(2);
+bst.insert(10);
+bst.insert(1);
+bst.insert(5);
+bst.insert(12);
+bst.insert(8);
+bst.insert(11);
+        
+console.log(bst)
