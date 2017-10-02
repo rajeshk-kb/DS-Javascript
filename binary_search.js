48230cd64d3e1a7e268d8d3f708b16aa6e4642c5
User <user@Apples-MacBook-Pro.local>
2017-10-02 17:49:44 +0530

    binary search and matrix rotation

---
+++ binary_search.js
@@ -0,0 +1,30 @@
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14, 15, 16, 17];
+
+var first = 0;
+var last = arr.length-1;
+var search = 17;
+var serchedElement = binarySearch(arr, first, last, search);
+console.log(serchedElement)
+
+
+
+function binarySearch(arr, first, last, search){
+    if(last <= 0){
+        return -1;
+    }
+    var mid = first+last;
+    if(search == arr[mid]){
+        return mid;
+    }
+    else if(search<arr[mid]){
+        last = mid-1;
+        binarySearch(arr, first, last, search)
+    }
+    else if(search>arr[mid]){
+        first = mid+1;
+        binarySearch(arr, first, last, search)
+    }else{
+        return 0;
+    }
+}
---
+++ matrix_rotation.js
@@ -0,0 +1,53 @@
+/* Rotate a matrix by 90 degree */
+var matrix = [];
+(function () {
+
+    var k = 1;
+    for (var i = 0; i < 5; i++) {
+        var mat = [];
+        for (var j = 0; j < 5; j++) {
+            mat.push(k);
+            k++;
+        }
+        matrix.push(mat);
+    }
+
+})();
+
+console.log("--------  Matrix -----")
+console.log(matrix)
+transposeOfMatrix(matrix);
+
+
+function transposeOfMatrix(matrix) {
+
+    var row = matrix.length;
+    var col = matrix.length;
+    for (var i = 0; i < row; i++) {
+        for (var j = i; j < col; j++) {
+            var temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp;
+        }
+    }
+
+    console.log("-------- Transpose of matrix -----")
+    console.log(matrix)
+    reverseColms(matrix)
+}
+
+function reverseColms(matrix) {
+
+    var row = matrix.length;
+    var col = matrix.length;
+    for (var i = 0; i < reverseColms; i++) {
+        for (var j = 0; j < row / 2; j++) {
+            var temp = matrix[j][i];
+            matrix[j][i] = matrix[row - 1 - j][i];
+            matrix[row - 1 - j][i] = temp;
+        }
+    }
+    console.log("-------- Reversed matrix -----")
+    console.log(matrix)
+}
+
