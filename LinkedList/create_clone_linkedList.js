bf3d2226ec31d5f9c448c568bafcd2a5fe213202
Rajesh Kumar <rajeshkumar11cse@gmail.com>
2019-08-30 12:39:52 +0530

    clone linked list with random pointer

--- LinkedList/create_clone_linkedList.js
+++ LinkedList/create_clone_linkedList.js
@@ -1,10 +1,7 @@
 
-var util = require('util')
-
 function LikedList() {
     this.head = null;
 }
-
 
 LikedList.prototype.push = function (val) {
     let node = {
@@ -28,19 +25,17 @@ LikedList.prototype.clone = function () {
 
     var orig_curr = this.head;
     var clone_curr = null;
-
     while (orig_curr !== null) {
-
         let node = {
             data: orig_curr.data,
             next: orig_curr.next,
             random: orig_curr.random,
         }
-
         clone_curr = node;
-        map.set(orig_curr, clone_curr);
+        map.set(orig_curr, clone_curr); //  Set original node as key and cloned node as value
         orig_curr = orig_curr.next;
     }
+    return map;
 }
 
 let ll = new LikedList();
@@ -56,19 +51,28 @@ ll.push(1);
 ll.head.random = ll.head.next.next;
 ll.head.next.random = ll.head.next.next.next;
 ll.head.next.next.random = ll.head.next.next.next.next;
-ll.head.next.next.next.random = ll.head.next.next.next.next.next;
+ll.head.next.next.next.random = ll.head.next.next;
 ll.head.next.next.next.next.random = ll.head.next;
-
 
 // Making a clone of the original linked list.
 var clone = ll.clone();
 
-console.log(map);
 
-   //console.log(util.inspect(map, { showHidden: true, depth: null, colors: true }))
+clone.forEach(function(value, key){
+    console.log(value.data, value.random ? value.random.data:null, '--', key.data, key.random ? key.random.data:null);
+});
+
+// Output
+//  Key -- Value
+// 1 3 '--' 1 3
+// 2 4 '--' 2 4
+// 3 5 '--' 3 5
+// 4 3 '--' 4 3
+// 5 2 '--' 5 2
 
 
-
+// Time Complexity:  O(n)
+// Auxiliary Space:  O(n)
 
 
 
