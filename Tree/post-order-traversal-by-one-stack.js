d85bf831c70727bee327d063ca06239481eafc2c
Varsha Chahil <varshachahil@Varshas-MacBook-Air.local>
2020-01-11 16:02:06 +0530

    post order traveral by one stack

---
+++ Tree/post-order-traversal-by-one-stack.js
@@ -0,0 +1,92 @@
+
+
+let BST = require('./create_binary_tree.js');
+
+
+postOrderTraversalByOneStack(BST.root);
+
+
+
+
+
+
+
+
+function postOrderTraversalByOneStack(root){
+
+	var p = root;
+
+	var stack = new Array();
+
+	while(true){
+		if(p){
+			stack.push(p);
+			p = p.left;
+		}else{
+
+			if(stack.length == 0){
+				break;
+			}else{
+
+				var top = stack.top();
+
+				if(Top.right === null){
+
+					p = stack.pop();
+
+					console.log(p.data);
+
+					while(p == Top.right){
+
+						console.log(Top.data);
+						stack.pop();
+					}
+				}
+
+				if(stack.length !== 0){
+					p = top.right;
+
+				}else{
+					p = null;
+				}
+			}
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// Ref https://www.geeksforgeeks.org/iterative-postorder-traversal-using-stack/
+/// Ref https://www.youtube.com/watch?v=cviyUv2TFG8
