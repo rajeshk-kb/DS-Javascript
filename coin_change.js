c98657f7c7b4cac9f114283ce67f291c19551473
Varsha Chahil <varshachahil@Varshas-MacBook-Air.local>
2020-01-11 21:17:14 +0530

    coin change update

--- coin_change.js
+++ coin_change.js
@@ -6,10 +6,11 @@
 
 
 
-var arr = [1, 2, 3];
-var N = 4;;
 
-var no_of_ways = count(arr, N, N);
+var arr = [1, 2, 3];
+var N = 4;
+
+var no_of_ways = count(arr, arr.length, N);
 
 console.log(no_of_ways)
 
@@ -19,29 +20,38 @@ console.log(no_of_ways)
 
 function count(arr, m, n) {
 
-    var table = new Array(n+1);
-    for (i = 0; i <=m; i++) {
-        table[i] = new Array(m+1);
-        for (j = 0; j <=m; j++) {
-            table[i][j] = 0;
-        }
+
+// Set value 0 in all boxes;
+var table = new Array(n+1);
+for (i = 0; i <=m; i++) {
+    table[i] = new Array(m+1);
+    for (j = 0; j <=n; j++) {
+        table[i][j] = 0;
     }
-
-    table[0][0] = 1;
-
-    for (var i = 1; i <= m; i++) {
-        for (var j = 0; j <= n; j++) {
-            if (i > j) {
-                table[i][j] = table[i - 1][j]
-            } else {
-                table[i][j] = table[i - 1][j] + table[i][j - i];
-            }
-        }
-    }
-    // console.log(table[3][0])
-    console.log('===============')
-    console.log(table)
-    var op = table[arr.length][m];
-
-    return op;
 }
+
+// // Base case (If given value is 0)  
+for(var i=0;i<m;i++) {
+    table[0][i] = 1;  
+ } 
+
+
+ for(var i=1;i<=m;i++) { 
+
+  for(var j=1;j<=n;j++) { 
+      if(arr[i-1]>j) { 
+          table[i][j]=table[i-1][j]; 
+
+      } else{ 
+          table[i][j]=table[i-1][j]+table[i][j-(i-1)]; 
+      } 
+
+  } 
+} 
+var op = table[m][n];
+
+return op;
+}
+
+
+
