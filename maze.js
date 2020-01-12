249a9adf85d22f41b45cdbd417b048fbb118cb5b
Varsha Chahil <varshachahil@Varshas-MacBook-Air.local>
2020-01-12 22:33:37 +0530

    issue fix in maze

--- maze.js
+++ maze.js
@@ -3,29 +3,25 @@
 
 var util = require('util')
 
-// 0 means unassigned cells
 var board =    [[1, 0, 0, 0],
                 [1, 1, 0, 1],
                 [0, 1, 1, 0],
                 [1, 0, 1, 1]];
 
 var N = 4;
-// 0 means unassigned cells
 var sol =  [[0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0]];
 
 // console.log(util.inspect(board, { showHidden: true, colors: true }))
-console.log('\x1b[31m%s\x1b[0m', "=================")
 function solveNQUtil(board, row, col) {
     if (col == N - 1 && row == N - 1) {
-        sol[col][col] = 7;
+        sol[row][col] = 7;
         console.log(util.inspect(sol, { showHidden: true, colors: true }))
         return true; // remove this in case of finding all possible solutions
     }
 
-    //for(var i = 0; i< N; i++){
     if (isSafe(board, row, col)) {
         sol[row][col] = 7;
 
@@ -36,23 +32,17 @@ function solveNQUtil(board, row, col) {
             return true;
         }
         sol[row][col] = 0;
-       // return false;
     }
-
     return false;
-    // }
 }
 
 solveNQUtil(board, 0, 0, sol);
 
 
-
 function isSafe(board, x, y) {
     var i, j;
-    // if (x,y outside maze) return false
     if (x >= 0 && x < N && y >= 0 && y < N && board[x][y] == 1)
         return true;
-
     return false;
 }
 
