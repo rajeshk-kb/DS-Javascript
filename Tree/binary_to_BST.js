3d55223e53a28b5de1740355766b45f13640bb62
RAJESH KUMAR <rajeshkumar11cse@gmail.com>
2020-01-13 18:35:49 +0530

    Update binary_to_BST.js

--- Tree/binary_to_BST.js
+++ Tree/binary_to_BST.js
@@ -5,7 +5,13 @@
     this.right = null;
   }
 
-  root = new newNode(10);
+
+  // index pointer..
+  function indexPointer() {
+    this.index = 0;
+  }
+
+  var root = new newNode(10);
   root.left = new newNode(30);
   root.right = new newNode(15);
   root.left.left = new newNode(20);
@@ -13,53 +19,51 @@
 
 
 
-//Steps ..
-// 1. Store inorder traversal to array 
-// 2. Sort array, sorted array represests as BST.
-// 3. Copy array elements back to Binary Tree through inorder traversal
+  //Steps ..
+  // 1. Store inorder traversal to array 
+  // 2. Sort array, sorted array represests as BST.
+  // 3. Copy array elements back to Binary Tree through inorder traversal
 
   console.log("Binary Tree - ", root);
   binaryTreeToBST(root);
   console.log("Binary Search Tree(BST) - ", root);
 
 
-
-
-function storeInorder(root, arr) {
-  // Base Case 
-  if (root == null) {
-    return;
+  function storeInorder(root, arr) {
+    // Base Case 
+    if (root == null) {
+      return;
+    }
+    storeInorder(root.left, arr)
+    arr.push(root.value);
+    storeInorder(root.right, arr)
   }
-  storeInorder(root.left, arr)
-  arr.push(root.value);
-  storeInorder(root.right, arr)
-}
 
-var INDEX = 0
-function arrayToBST(root, arr) {
-  // Base Case 
-  if (root == null) {
-    return;
+  function arrayToBST(root, arr, indexP) {
+    // Base Case 
+    if (root == null) {
+      return;
+    }
+    arrayToBST(root.left, arr, indexP)
+    root.value = arr[indexP.index];
+    indexP.index = indexP.index + 1;
+    arrayToBST(root.right, arr, indexP)
   }
-  arrayToBST(root.left, arr)
-  root.value = arr[INDEX];
-  INDEX = INDEX+1;
-  arrayToBST(root.right, arr)
-}
 
-function binaryTreeToBST(root) {
+  function binaryTreeToBST(root) {
 
-  if (root == null) {
-    return root;
+    if (root == null) {
+      return root;
 
+    }
+    var arr = [];
+    // store inorder traversal of tree in arr
+    storeInorder(root, arr);
+
+    // Sort Array
+    arr.sort((a, b) => { return a - b })
+
+    // Copy array elements back to Binary Tree 
+    var index = new indexPointer();
+    arrayToBST(root, arr, index);
   }
-  var arr = [];
-  // store inorder traversal of tree in arr
-  storeInorder(root, arr);
-
-  // Sort Array
-  arr.sort((a, b)=>{return a-b})
- 
-  // Copy array elements back to Binary Tree 
-  arrayToBST(root, arr); 
-}
