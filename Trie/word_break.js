f3fae272cf11bab59a70ee97c6e38c9bd7997615
Varsha Chahil <varshachahil@Varshas-MacBook-Air.local>
2020-02-05 23:08:34 +0530

    word break problem by DP and Trie

--- Trie/word_break.js
+++ Trie/word_break.js
@@ -51,3 +51,47 @@ function search(root, word) {
  Search() && WordBreak()
   (T)'their'     (T)
 */
+
+
+
+//Dynamic Programming -- Word Break Problem
+
+var dictionary = ["mobile",
+ "samsung", "sam", "sung", "man", 
+ "mango", "icecream", "and", "go", "i", 
+ "like", "ice", "cream"];
+
+var output = wordBreakDP("ilikesamsung", dictionary) ? "Yes" : "No";
+console.log(output)
+
+var i = 0;
+function wordBreakDP(str, dict) {
+    let size = str.length;
+
+    if (size == 0)
+        return true;
+
+    for (let i = 1; i <= size; i++) {
+      We first check whether 
+        // First check whether current prefix is in dictionary. Then recursively check for remaining 
+        // string str.substr(i, size) which is suffix of length size
+        if(dictionaryContains(dict, str.substring(0, i)) && wordBreakDP(str.substring(i, size), dict)){ // recursion
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function dictionaryContains(dict, word) {
+    var size = dict.length;
+    for (var i = 0; i < size; i++)
+        if (dict[i] === word)
+            return true;
+    return false;
+}
+
+
+
+
+
